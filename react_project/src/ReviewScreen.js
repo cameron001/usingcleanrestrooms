c10e33e5e057b7068085dfea283f47ee1b42275b
CalebMCode <caleb.n.mcclure@gmail.com>
2023-03-08 16:59:30 -0800

    Added getting reviews from sheets

--- react_project/src/ReviewScreen.js
+++ react_project/src/ReviewScreen.js
@@ -1,29 +1,56 @@
-import React, { useState } from "react";
-import { View, Text } from 'react-native';
-import styleSheet from './styles.js';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-// Shows after marker click
-const ReviewScreen = ({navigation, route}) => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={styleSheet.titleText}>
-        {route.params.name} Restroom Reviews
-      </Text>
-      <br></br>
-      <br></br>
-      <Text style={styleSheet.reviewText}>
-        John Doe: 4/5
-      </Text>
-      <br></br>
-      <Text style={styleSheet.reviewText}>
-        Jane Doe: 5/5
-      </Text>
-      <br></br>
-      <Text style={styleSheet.reviewText}>
-        George Hill: 3/5
-      </Text>
-    </View>
-  )
-};
+export function ReviewScreen() {
+    const [data, setData] = useState([]);
 
-export default ReviewScreen;
+    const fetchData = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var requestOptions = {
+            method: "get",
+            headers: myHeaders,
+            redirect: "follow",
+
+        };
+        fetch("https://v1.nocodeapi.com/calebmcclure/google_sheets/sfwrdAWrqvFOoVFt?tabId=FormResponses&filterBy=Building&filterValue=SRC", requestOptions)
+            .then(response => response.json())
+            .then((actualData) => {
+                console.log(actualData);
+                setData(actualData.data);
+                console.log(data);
+            }
+            );
+    };
+
+useEffect(() => {
+    fetchData();
+}, []);
+
+    return (
+        <div className="ReviewScreen">
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Bathroom</th>
+                    <th>Floor</th>
+                    <th>Accessible</th>
+                    <th>Rating</th>
+                    <th>Review</th>
+                </tr>
+                {data.map((review, index) => (
+                    <tr key={index}>
+                        <td>{review.Name}</td>
+                        <td>{review.Building}</td>
+                        <td>{review.Floor}</td>
+                        <td>{review.Accessible}</td>
+                        <td>{review.Rating}</td>
+                        <td>{review.Review}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </div>
+    );
+}
+
+export default ReviewScreen
